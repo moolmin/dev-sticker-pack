e5e3400543eb6ba1a76ee91dbb99f4799d328730
Sumin Kim <zzmzzm0065@gmail.com>
2024-12-08 21:59:12 +0900

    feat: create card board and sticker container

---
+++ src/components/CardBoard/CardBoard.jsx
@@ -0,0 +1,17 @@
+import './CardBoard.scss'
+
+export default function CardBoard() {
+  return (
+    <div className='card-board-container'>
+        <div className='hole-container'>
+            <div className='small-hole'></div>
+            <div className='big-hole'></div>
+        </div>
+        <div className='text-container'>
+            <div className='programming'>Programming</div>
+            <h1>Sticker Pack</h1>
+        </div>
+
+    </div>
+  )
+}
---
+++ src/components/StickerPack/StickerPack.jsx
@@ -0,0 +1,11 @@
+import "./StickerPack.scss";
+import CardBoard from "../CardBoard/CardBoard";
+
+export default function StickerPack() {
+  return (
+    <div className="pack-container">
+        <CardBoard />
+      <div className="sticker-container">dfsf</div>
+    </div>
+  );
+}
