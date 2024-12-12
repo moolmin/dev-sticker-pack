ad43802166d40eace3c6d63b63556579ee37d92e
Sumin Kim <zzmzzm0065@gmail.com>
2024-12-12 21:13:33 +0900

    chore: add sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,6 +2,10 @@ import './SideBar.scss'
 
 export default function SideBar() {
   return (
-    <div className='sidebar-container'>SideBar</div>
+    <div className='sidebar-container'>
+      <h1>Developer's Sticker Pack</h1>
+      <h3>Download programming icons for free</h3>
+      <p>Copyright&copy; 2024 <a href="https://github.com/moolmin">moolmin</a></p>
+      </div>
   )
 }
